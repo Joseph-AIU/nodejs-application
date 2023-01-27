08da357ac7d61d6768a4248f63149c6264cd341b
Joseph-AIU <120756372+Joseph-AIU@users.noreply.github.com>
2023-01-27 05:01:16 -0600

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,12 +1,12 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://172.31.13.99:9000/',
+  serverUrl: 'http://172.31.5.162:5000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsAcadalearning',
-	    'sonar.login':'36166db504bf731e392b60a0ea6e196c6567b58e',
+	    'sonar.login':'squ_9f57424df50a59249f3d01b3eb3837360c10b803',
 	    //'sonar.password': 'admin',
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
